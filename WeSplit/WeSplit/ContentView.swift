2be41be31fe058b46d4fa2a262abc3d2189b90c3
Samantha Gatt <smfgatt@gmail.com>
2020-01-07 19:13:33 -0600

    Resets project

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,35 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var tapCount = 0
-    @State private var name = ""
-    @State private var selectedStudent = "Harry"
-    
-    private let students = ["Harry", "Hermione", "Ron"]
-    
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Button("Tap count: \(tapCount)") {
-                        self.tapCount += 1
-                    }
-                }
-                Section {
-                    TextField("Enter your name", text: $name)
-                    Text("Hello \(name)")
-                }
-                Section {
-                    Picker("Select student", selection: $selectedStudent) {
-                        ForEach(0..<students.count) {
-                            Text(self.students[$0])
-                        }
-                    }
-                }
-            }
-            .navigationBarTitle(Text("SwiftUI"))
-        }
+        Text("Hello world")
     }
 }
 
