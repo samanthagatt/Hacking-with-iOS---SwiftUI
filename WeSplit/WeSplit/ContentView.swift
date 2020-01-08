0d8319cef32e472e477e9c4642a492d2bfbfc7de
Samantha Gatt <smfgatt@gmail.com>
2020-01-07 19:42:47 -0600

    Adds a picker view for number of people

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -17,11 +17,19 @@ struct ContentView: View {
     private let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Check Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Check Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
             }
+            .navigationBarTitle("WeSplit")
         }
     }
 }
