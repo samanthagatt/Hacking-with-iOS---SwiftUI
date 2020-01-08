0d3d875731b256128fb4c010b604873b9a3b6767
Samantha Gatt <smfgatt@gmail.com>
2020-01-07 19:23:51 -0600

    Creates text field for check amount

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,8 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    private let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello world")
+        Form {
+            Section {
+                TextField("Check Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+            }
+        }
     }
 }
 
