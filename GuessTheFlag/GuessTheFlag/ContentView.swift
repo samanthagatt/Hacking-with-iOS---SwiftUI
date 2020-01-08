8fb0c854c08282be111b4f1347d0312bd4f6867b
Samantha Gatt <smfgatt@gmail.com>
2020-01-08 00:55:13 -0600

    Creates3x3 stack views

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,7 +10,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20) {
+            HStack(alignment: .center, spacing: 20) {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack(spacing: 20) {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack(spacing: 20) {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
     }
 }
 
