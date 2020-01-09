a6d6088f0e72554a2f2a7a7709bd9fcb74bdaebd
Samantha Gatt <smfgatt@gmail.com>
2020-01-09 16:31:53 -0600

    Creates a wake up time date picker

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,11 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount: Double = 8
     
-    var body: some View {
+    @State private var sleepAmount: Double = 8
+    @State private var wakeUp = Date()
+    
+    private var sleepAmountStepper: some View {
         Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
             Text("\(sleepAmount, specifier: "%g") hours")
+        }
+    }
+    private var wakeUpDatePicker: some View {
+        DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+    }
+    
+    var body: some View {
+        VStack {
+            sleepAmountStepper
+            wakeUpDatePicker
         }
     }
 }
