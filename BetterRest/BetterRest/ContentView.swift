6f59f9694b059b68a4d5d4976e1992d8cb8234c0
Samantha Gatt <smfgatt@gmail.com>
2020-01-10 17:41:43 -0600

    Touches up UI

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -10,29 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var wakeUpDate = Date()
+    // MARK: State properties
+    @State private var wakeUpDate: Date = {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 30
+        return Calendar.current.date(from: components) ?? Date()
+    }()
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     @State private var calculationTitle = ""
     @State private var calculationDescription = ""
     @State private var calculationShowing = false
     
+    // MARK: General properties
     private var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.timeStyle = .short
         return formatter
     }()
     
+    // MARK: Subviews
     private var wakeUpStack: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 0) {
             Text("When do you want to wake up?")
                 .font(.headline)
             DatePicker("Please enter a time", selection: $wakeUpDate, displayedComponents: .hourAndMinute)
-            .labelsHidden()
+                .labelsHidden()
+                .datePickerStyle(WheelDatePickerStyle())
         }
     }
     private var sleepAmountStack: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 0) {
             Text("Desired amount of sleep")
                 .font(.headline)
             Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
@@ -41,7 +50,7 @@ struct ContentView: View {
         }
     }
     private var coffeeAmountStack: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 0) {
             Text("Daily coffee intake")
                 .font(.headline)
             Stepper(value: $coffeeAmount, in: 0...20) {
@@ -55,9 +64,10 @@ struct ContentView: View {
         }
     }
     
+    // MARK: Body
     var body: some View {
         NavigationView {
-            VStack(spacing: 20) {
+            Form {
                 wakeUpStack
                 sleepAmountStack
                 coffeeAmountStack
@@ -69,6 +79,7 @@ struct ContentView: View {
         }
     }
     
+    // MARK: Methods
     private func calculateBedtime() {
         let model = SleepCalculator()
         let dateComponents = Calendar.current.dateComponents([.hour, .minute], from: wakeUpDate)
