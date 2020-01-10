89a48ecf354955561d903b35e6bc0bfccb81f727
Samantha Gatt <smfgatt@gmail.com>
2020-01-10 16:58:52 -0600

    Sets up UI

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,8 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUpDate = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    private var wakeUpStack: some View {
+        VStack {
+            Text("When do you want to wake up?")
+                .font(.headline)
+            DatePicker("Please enter a time", selection: $wakeUpDate, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+        }
+    }
+    private var sleepAmountStack: some View {
+        VStack {
+            Text("Desired amount of sleep")
+                .font(.headline)
+            Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                Text("\(sleepAmount, specifier: "%g") hours")
+            }
+        }
+    }
+    private var coffeeAmountStack: some View {
+        VStack {
+            Text("Daily coffee intake")
+                .font(.headline)
+            Stepper(value: $coffeeAmount, in: 0...20) {
+                Text("\(coffeeAmount) \(coffeeAmount == 1 ? "cup" : "cups")")
+            }
+        }
+    }
+    private var calculateButton: some View {
+        Button(action: calculateBedtime) {
+            Text("Calculate")
+        }
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack(spacing: 20) {
+                wakeUpStack
+                sleepAmountStack
+                coffeeAmountStack
+            }.navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing: calculateButton)
+        }
+    }
+    
+    private func calculateBedtime() {
+        // TODO: implement function
     }
 }
 
