e2f0fd75eab4a0934125839f6fd8b0288734dc13
Samantha Gatt <smfgatt@gmail.com>
2020-01-13 13:08:47 -0600

    Adds different types of Lists

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,8 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let names = ["Samantha", "Sarah", "Stephanie", "Melanie"]
+    
+    private var staticList: some View {
+        List {
+            Section(header: Text(":)")) {
+                Text("Hello, world!")
+            }
+            Section(header: Text(":(")) {
+                Text("Goodbye, world.")
+            }
+        }.listStyle(GroupedListStyle()) // same style as `Form`
+    }
+    private var dynamicList: some View {
+        List(names, id: \.self) {
+            Text($0)
+        }
+    }
+    private var mixedList: some View {
+        List {
+            Section(header: Text("Section 1")) {
+                ForEach(names, id: \.self) {
+                    Text($0)
+                }
+            }
+            Section(header: Text("Section 2")) {
+                Text("Here's a static cell!")
+            }
+            Section(header: Text("Section 3")) {
+                Text("And another!")
+            }
+        }
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack() {
+            staticList
+            Color.orange.frame(height: 1)
+            mixedList
+            Color.orange.frame(height: 1)
+            dynamicList
+        }
     }
 }
 
