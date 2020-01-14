538061b55ea953236b9c8f29467a61ec2248725c
Samantha Gatt <smfgatt@gmail.com>
2020-01-14 16:38:20 -0600

    Mixes animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var buttonScale: CGFloat = 1.0
+    @State private var radarScale: CGFloat = 1.0
     
     var body: some View {
         Button("Tap me") {
@@ -19,6 +20,18 @@ struct ContentView: View {
         .background(Color.red)
         .foregroundColor(.white)
         .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                // starts at 1 then increases to 2 onAppear
+                .scaleEffect(radarScale)
+                // opacity starts at 1 then decreases to 0 onAppear
+                .opacity(Double(2 - radarScale))
+                .animation(
+                    Animation.easeOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
         .scaleEffect(buttonScale)
         .blur(radius: (buttonScale - 1) * 3)
         // .animation(.easeInOut(duration: 3))
@@ -27,6 +40,9 @@ struct ContentView: View {
                 .delay(0.5)
                 .repeatCount(3, autoreverses: true)
         )
+        .onAppear {
+            self.radarScale = 2
+        }
     }
 }
 
