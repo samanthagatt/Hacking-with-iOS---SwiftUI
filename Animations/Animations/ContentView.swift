6719f76a1434f1c14c516751cbed71c63a077c5f
Samantha Gatt <smfgatt@gmail.com>
2020-01-15 15:33:38 -0600

    Moves animation to variable binding
    Animation no longer on the view itself

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,36 +10,57 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // MARK: State variables
     @State private var buttonScale: CGFloat = 1.0
     @State private var radarScale: CGFloat = 1.0
     
-    var body: some View {
-        Button("Tap me") {
+    // MARK: Subviews
+    private var radarCircle: some View {
+        Circle()
+            .stroke(Color.red)
+            // starts at 1 then increases to 2 onAppear
+            .scaleEffect(radarScale)
+            // opacity starts at 1 then decreases to 0 onAppear
+            .opacity(Double(2 - radarScale))
+            .animation(
+                Animation.easeOut(duration: 1)
+                    .repeatForever(autoreverses: false)
+            )
+    }
+    private var animatedButton: some View {
+        Button(action: {
             self.buttonScale += 1.0
-        }.padding(50)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.red)
-                // starts at 1 then increases to 2 onAppear
-                .scaleEffect(radarScale)
-                // opacity starts at 1 then decreases to 0 onAppear
-                .opacity(Double(2 - radarScale))
-                .animation(
-                    Animation.easeOut(duration: 1)
-                        .repeatForever(autoreverses: false)
-                )
-        )
-        .scaleEffect(buttonScale)
-        .blur(radius: (buttonScale - 1) * 3)
-        // .animation(.easeInOut(duration: 3))
-        .animation(
-            Animation.interpolatingSpring(stiffness: 30, damping: 4)
-                .delay(0.5)
-                .repeatCount(3, autoreverses: true)
-        )
+        }) {
+            Text("Tap me")
+                .padding(50)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .overlay(radarCircle)
+                .scaleEffect(buttonScale)
+                .blur(radius: (buttonScale - 1) * 3)
+        }
+    }
+    private var animationStepper: some View {
+        Stepper("Scale amount",
+                value: $buttonScale.animation(
+                    Animation.interpolatingSpring(stiffness: 30, damping: 4)
+                        .delay(0.5)
+                        .repeatCount(3, autoreverses: true)
+                    ),
+                in: 0.5...10,
+                step: 0.5)
+            .padding()
+    }
+    
+    // MARK: Content body
+    var body: some View {
+        VStack {
+            animationStepper
+            Spacer()
+            animatedButton
+            Spacer()
+        }
         .onAppear {
             self.radarScale = 2
         }
